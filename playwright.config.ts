1f16cda70f713c717d3649b54d36244717da5e31
Przemyslaw Horban <p.horban@invinets.com>
2024-01-10 17:16:09 +0100

    Initial

---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import type { PlaywrightTestConfig } from '@playwright/test'
+import { devices } from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    }
+  ],
+
+}
+
+export default config
